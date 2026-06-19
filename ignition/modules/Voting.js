776b81ff5ce358275304641c56993ca806117392
chamizzu <aiisyahrahmasarii@gmail.com>
2026-06-19 14:49:08 +0700

    edit durasi

--- ignition/modules/Voting.js
+++ ignition/modules/Voting.js
@@ -2,7 +2,7 @@ const { buildModule } = require("@nomicfoundation/hardhat-ignition/modules");
 
 module.exports = buildModule("VotingModule", (m) => {
   // Durasi default voting adalah 20 menit
-  const durationInMinutes = m.getParameter("durationInMinutes", 20);
+  const durationInMinutes = m.getParameter("durationInMinutes", 50);
 
   const voting = m.contract("Voting", [durationInMinutes]);
 
